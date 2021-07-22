a7a371112cc4a493816176a61fc006f952576677
iamfaiz123 <87696372+iamfaiz123@users.noreply.github.com>
2021-07-22 12:04:59 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,29 @@
+// this code is by iamfaizalkhn@gmail.com
+#include<iostream>
+void main()
+{
+  char Apple = "A";
+  if(Apple>=65 && Apple<97)
+  {
+    cout<<"enter char is in upper case;
+   }
+  else {
+    cout<<"it's in lower case
+   } 
+  //for strings to check is all or sum chars are in lower case
+  string banana;
+  gets(banana);
+  for (auto i=0;banana[i]!="/0";i++)
+  {
+    if(banana[i]>=65 && banana[i]<97)
+  {
+    cout<<"enter char is in upper case;
+   }
+  else {
+    cout<<"it's in lower case";
+// using this method you can also grab numbers in a string in c ,ie if the char char does not falls in range of 65 to 122;
+    //happy coding
+    
+  
+      
+  
